b335b7bc8558746346ed9daf72ceb59d9bd67ef6
IbrahimaTk-7 <ibrahimatounkangbalde@gmail.com>
2024-04-26 03:04:05 +0000

    First commit

---
+++ my-react-app/src/Footer.jsx
@@ -0,0 +1,13 @@
+
+function Footer(){
+
+    return(
+        <footer>
+            <p>
+                &copy; {new Date().getFullYear()} TK_Balde
+            </p>
+        </footer>
+    );
+}
+
+export default Footer
